76e1da5543e4dc8461a6d791a8cdf5fa3315676c
Shengjia Zhao <teckingzhao@qq.com>
2016-01-21 21:30:42 +0800

    Test bench to test performance of agents and plot results

--- sign.py
+++ sign.py
@@ -19,10 +19,14 @@ class Simulator:
         self.nsamples += 1
         if self.kind == 'bernoulli':
             if random.random() > (self.mean + 1.0) / 2:
-                return -1
+                return -0.5
             else:
-                return 1
+                return 0.5
 
+    def finish(self):
+        copy = self.nsamples
+        self.nsamples = 0
+        return copy
 
 class ExpGap:
     def __init__(self):
@@ -52,7 +56,7 @@ class LILTest:
         pass
 
     def run(self, sim, delta):
-        a = 0.8
+        a = 0.6
         c = 1.1
         b = (math.log(zeta(2 * a / c, 1)) - math.log(delta)) * c / 2
         print(a, b, c)
@@ -63,13 +67,40 @@ class LILTest:
             sum += sim.sim()
             n += 1.0
             boundary = math.sqrt(a * n * math.log(math.log(n, c) + 1) + b * n)
-            print(sum, boundary)
-
             if sum >= boundary:
                 return True
             elif sum <= -boundary:
                 return False
 
+class TestBench:
+    def __init__(self, size):
+        self.test_size = size
+
+    def test(self, agent):
+        mean_range = 22
+        mean_array = np.zeros(mean_range)
+        accuracy = np.zeros(mean_range)
+        average_sample = np.zeros(mean_range)
+        majority_sample = np.zeros(mean_range)
+        for mean_index in range(0, mean_range):
+            mean = math.exp(-0.2 * mean_index)
+            print("Testing on Bernoulli: mean = " + str(mean))
+            correct_count = 0
+            sample_count = np.zeros(self.test_size)
+            for iter in range(0, self.test_size):
+                if random.random() > 0.5:
+                    mean = -mean
+                sim = Simulator(kind="bernoulli", mean=mean)
+                result = agent.run(sim, 0.05)
+                if (result is True and mean > 0) or (result is False and mean < 0):
+                    correct_count += 1
+                sample_count[iter] = sim.finish()
+
+            mean_array[mean_index] = math.fabs(mean)
+            accuracy[mean_index] = float(correct_count) / self.test_size
+            average_sample[mean_index] = np.sum(sample_count) / self.test_size
+            majority_sample[mean_index] = np.sort(sample_count)[int(self.test_size * 0.9)]
+        return mean_array, accuracy, average_sample, majority_sample
 
 if __name__ == '__main__':
     sim = Simulator(kind='bernoulli', mean=-0.1)
@@ -80,3 +111,27 @@ if __name__ == '__main__':
     lil_agent = LILTest()
     lil_agent.run(sim, 0.1)
     print(sim.finish())
+
+    test = TestBench(200)
+    mean, accuracy, average_sample, majority_sample = test.test(lil_agent)
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ax2 = ax1.twinx()
+    plt.title('Accuracy and Required Samples vs. Mean')
+    lns1 = ax1.scatter(mean, accuracy, c='r', label='accuracy')
+    lns2 = ax2.plot(mean, average_sample, c='g', label='average sample usage')
+    lns3 = ax2.plot(mean, majority_sample, c='b', label='top 10% sample usage')
+    ax2.set_yscale('log')
+    ax1.set_xscale('log')
+    ax2.set_xscale('log')
+    ax1.set_xlim((0.01, 1))
+    ax2.set_xlim((0.01, 1))
+    ax1.set_xlabel('mean')
+    ax1.set_ylabel('accuracy')
+    ax2.set_ylabel('samples used')
+    lns = [lns1] + lns2 + lns3
+    labs = [l.get_label() for l in lns]
+    plt.legend(lns, labs, loc='lower right')
+    plt.show()
+
