993d729e9e44422564d5100018e1e2bb5a02dd0e
Shengjia Zhao <teckingzhao@qq.com>
2016-02-02 16:03:57 +0800

    Added sparse AH option for AHAgent

--- mab.py
+++ mab.py
@@ -18,12 +18,12 @@ class Simulator:
         self.K = nbr_arms
         self.nsamples = 0
         self.kind = kind
-        self.sample_size = np.zeros(K)
+        self.sample_size = np.zeros(self.K)
         self.dist = dist
-        self.mean_list = np.zeros(K)
+        self.mean_list = np.zeros(self.K)
 
         # Compute the mean for each arm
-        for arm in range(0, K):
+        for arm in range(0, self.K):
             if self.kind == 'H0':
                 if arm == 0:
                     self.mean_list[arm] = 0.6
@@ -79,7 +79,7 @@ class MABTestBench:
     def __init__(self, kind, size):
         self.kind = kind
         self.size = size
-        self.k_range = 7
+        self.k_range = 10
 
     def test(self, agent):
         """ Run the test on an agent and return the number of arms, average accuracy, average number of samples, and number
@@ -108,7 +108,7 @@ class MABTestBench:
             accuracy[i] = float(correct_count) / self.size
             average_sample[i] = np.sum(sample_count) / self.size / sim.hardness()
             majority_sample[i] = np.sort(sample_count)[int(self.size * 0.9)] / sim.hardness()
-            K *= 2
+            K = int(K * 1.5)
         return narms_array, accuracy, average_sample, majority_sample
 
     def test_and_plot(self, agent):
@@ -203,6 +203,8 @@ class BanditAgent:
         for item in right_objects:
             self.ax2.scatter(x_index, item, color=color_list[color_count % len(color_list)])
             color_count += 1
+        self.ax1.set_ylim((0, 1))
+        self.ax2.set_ylim((0, 1))
         plt.draw()
         time.sleep(0.001)
 
@@ -236,8 +238,8 @@ class LILUCBAgent(LILAgent):
         LILAgent.__init__(self, confidence)
 
     def run(self, sim, plot=False):
-        beta = 1
-        alpha = 9
+        beta = 1.0
+        alpha = 9.0
         delta = self.delta
 
         if plot:
@@ -321,12 +323,18 @@ class LILLUCBAgent(LILAgent):
 class LILAEAgent(BanditAgent):
     """ Agent that uses LIL-AE algorithm """
 
-    def __init__(self, confidence):
+    def __init__(self, confidence, stop_interval=1):
         BanditAgent.__init__(self)
-        self.a = 0.6
-        self.c = 1.1
         self.confidence = confidence / 2
-        self.b = (math.log(zeta(2 * self.a / self.c, 1)) - math.log(confidence)) * self.c / 2
+        self.stop_interval = stop_interval
+        if stop_interval != 1:
+            self.a = 0.55
+            self.c = self.stop_interval
+            self.b = (math.log(zeta(2 * self.a, 1)) - math.log(confidence)) / 2
+        else:
+            self.a = 0.6
+            self.c = 1.1
+            self.b = (math.log(zeta(2 * self.a / self.c, 1)) - math.log(confidence)) * self.c / 2
 
     def run(self, sim, plot=False):
         if plot:
@@ -341,17 +349,28 @@ class LILAEAgent(BanditAgent):
             sample_count[i] += 4
 
         counter = 0
+        next_stop = 1
         while True:
+            counter += 1
             mean = mean_array / sample_count
+            if self.stop_interval == 1:
+                conf_c = self.c
+            else:
+                conf_c = 1.0
             boundary = np.sqrt(self.a * sample_count * np.log(np.log(sample_count) / np.log(self.c) + 1)
                                + self.b * sample_count) / sample_count
             n = sample_count[np.argmax(mean)]
             boundary[np.argmax(mean)] = np.sqrt(self.a * n * math.log(math.log(n, self.c) + 1) +
-                                                (self.b + self.c * math.log(mean_array.size) / 2) * n) / n
+                                                (self.b + conf_c * math.log(mean_array.size) / 2) * n) / n
             ucb = mean + boundary
             lcb = mean - boundary
             maxlcb = lcb[np.argmax(mean)]
 
+            if self.stop_interval != 1:
+                if counter < next_stop:
+                    continue
+                while next_stop < n + 1:
+                    next_stop *= self.stop_interval
             if np.min(ucb) <= maxlcb:
                 for k in range(0, mean_array.size):
                     if ucb[k] <= maxlcb:
@@ -369,7 +388,6 @@ class LILAEAgent(BanditAgent):
             mean_array[cur_sample] += sim.sim(index[cur_sample])
             sample_count[cur_sample] += 1
 
-            counter += 1
             if plot and counter % 20 == 0:
                 self.draw(index, [ucb, mean, lcb], [sample_count])
 
@@ -430,16 +448,126 @@ class LILLSAgent(LILAgent):
                 self.draw(range(0, sim.K), [explore_val, ucb, mean], [sample_count])
 
 
+class MedianAgent(BanditAgent):
+    def __init__(self, confidence):
+        BanditAgent.__init__(self)
+        self.confidence = confidence
+
+    def run(self, sim):
+        arms = np.zeros(sim.K)
+        for i in range(0, sim.K):
+            arms[i] = i
+        r = 0
+        h = 1
+        self.init_display()
+        while True:
+            r += 1
+            if arms.size == 1:
+                return arms[0]
+            epsilon_r = 2 ** (-r)
+            delta_r = self.confidence / 50 / (r ** 2)
+            print("Step 1")
+            a_r = self.median_elimination(sim, arms, epsilon_r / 4, 0.01)
+            print("Step 2")
+            mu_a_r = self.uniform_sampling(sim, np.asarray([a_r]), epsilon_r / 4, delta_r)[0]
+            print("Step 3")
+            if self.frac_test(sim, arms, mu_a_r - 1.5 * epsilon_r, mu_a_r - 1.25 * epsilon_r, delta_r, 0.4, 0.1):
+                print("Step 4")
+                delta_h = self.confidence / 50 / (h ** 2)
+                b_r = self.median_elimination(sim, arms, epsilon_r / 4, delta_h)
+                print("Step 5")
+                mu_b_r = self.uniform_sampling(sim, np.asarray([b_r]), epsilon_r / 4, delta_h)[0]
+                print("Step 6")
+                arms = self.elimination(sim, arms, mu_b_r - 0.5 * epsilon_r, mu_b_r - 0.25 * epsilon_r, delta_h)
+                h += 1
+            print("One iteration completed, with " + str(arms.size) + " arms left")
+            self.draw(arms, np.zeros(arms.size), np.ones(arms.size))
+            time.sleep(2)
+
+    def uniform_sampling(self, sim, arms, epsilon, delta):
+        repeat = int(2 / (epsilon ** 2) * math.log(2 / delta))
+        mean_array = np.zeros(arms.size)
+        for index in range(0, arms.size):
+            for i in range(0, repeat):
+                mean_array[index] += sim.sim(arms[index])
+            mean_array[index] /= repeat
+        return mean_array
+
+    def frac_test(self, sim, arms, c_l, c_r, delta, t, epsilon):
+        count = 0.0
+        total = int(math.log(2 / delta) / ((epsilon / 3) ** 2) / 2)
+        for i in range(0, total):
+            arm = random.choice(arms)
+            mu = self.uniform_sampling(sim, np.asarray([arm]), (c_r - c_l) / 2.0, epsilon / 3.0)[0]
+            if mu < (c_l + c_r) / 2:
+                count += 1.0
+        if count / float(total) > t:
+            return True
+        else:
+            return False
+
+    def elimination(self, sim, arms, c_l, c_r, delta):
+        c_m = (c_l + c_r) / 2.0
+        r = 0
+        while True:
+            r += 1
+            delta_r = delta / 10.0 / (2.0 ** r)
+            print("Ministep " + str(r))
+            if self.frac_test(sim, arms, c_l, c_m, delta_r, 0.075, 0.025):
+                mean_array = self.uniform_sampling(arms, (c_r-c_m) / 2, delta_r)
+                thresh = (c_m + c_r) / 2
+                next_arms = []
+                for arm, mean in zip(arms, mean_array):
+                    if mean > thresh:
+                        next_arms.append(arm)
+                arms = np.asarray(next_arms)
+            else:
+                return arms
+
+    def median_elimination(self, sim, arms, epsilon, delta):
+        epsilon /= 4.0
+        delta /= 2.0
+        while True:
+            mean_array = np.zeros(arms.size)
+            repeat = int(1.0 / ((epsilon / 2.0) ** 2) * math.log(3.0/delta))
+            for index, arm in zip(range(0, arms.size), arms):
+                for rep in range(0, repeat):
+                    mean_array[index] += sim.sim(arm)
+                mean_array[index] /= repeat
+                # self.draw(arms, [mean_array], [mean_array])
+            sorted_array = mean_array.copy()
+            sorted_array.sort()
+            median = sorted_array[mean_array.size / 2]    # This selects median rounded up
+
+            # self.draw(arms, [mean_array], [np.ones(arms.size) * median])
+            next_arms = []
+            for index in range(0, arms.size):
+                if mean_array[index] >= median:
+                    next_arms.append(arms[index])
+            arms = np.asarray(next_arms)
+
+            if arms.size == 1:
+                break
+            epsilon = epsilon * 3.0 / 4.0
+            delta /= 2.0
+        return arms[0]
+
+
 if __name__ == '__main__':
     ae_agent = LILAEAgent(0.05)
     ucb_agent = LILUCBAgent(0.05)
     ls_agent = LILLSAgent(0.05)
     lucb_agent = LILLUCBAgent(0.05)
+    median_agent = MedianAgent(0.05)
+    eae_agent = LILAEAgent(0.05, stop_interval=1.05)
+    #test_sim = Simulator(20)
+    #eae_agent.run(test_sim, plot=True)
+    #median_agent.run(test_sim)
+    #median_agent.median_elimination(test_sim, np.asarray(range(0, 20)), 0.1, 0.05)
+    test = MABTestBench(kind='H2', size=1)
 
-    test = MABTestBench(kind='H2', size=20)
-
-    agents = [ae_agent, ucb_agent, ls_agent, lucb_agent]
-    labels = ['LIL-AE', 'LIL-UCB', 'LIL-UCB + LS', 'LIL-LUCB']
+    agents = [ae_agent, eae_agent, ucb_agent, ls_agent, lucb_agent]
+    labels = ['AHR', 'ES-AHR', 'LIL-UCB', 'LIL-UCB + LS', 'LIL-LUCB']
     test.compare_and_plot(agents, labels)
 
 
